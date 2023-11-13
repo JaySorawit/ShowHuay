128fa73af975f833198aa71455fa56600d320dc9
Jay Sorawit <ganglovenok@gmail.com>
2023-11-13 12:17:44 +0700

    fix navbar

--- frontend/src/user/Navbar.jsx
+++ frontend/src/user/Navbar.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   return (
     <nav>
-        <div className="container">
+        <div className="navcontainer">
             <div className="nav_logo">
                 <Link to='/'><img src={logo} alt="Logo" /></Link>
                 <Link to='/'><p>ShowHuay</p></Link>
