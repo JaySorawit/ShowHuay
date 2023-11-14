0b4a2af2221e9ab07a7ded4c470f7406fa928035
Jay Sorawit <sorawit.mokt@gmail.com>
2023-11-14 23:48:20 +0700

    fix chat.js

--- backend/app.js
+++ backend/app.js
@@ -1,31 +1,30 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const cors = require('cors');
+
 
 // Create Express app
 const app = express();
 
+// Enable CORS for all routes
+app.use(cors());
+
 // Create HTTP server
 const server = http.createServer(app);
 
+// Create Socket.io server
 const io = socketIo(server);
-io.on('connection', (socket) => {
-    console.log('A user connected');
-  
-    // Handle WebSocket events here
-    socket.on('disconnect', () => {
-      console.log('User disconnected');
-    });
-  });
 
 // Routes
 const chatRoutes = require('./routes/chatRoutes');
 
 app.use('/chat', chatRoutes(io));
 
+
 // Start the server
-app.listen(3000, () => {
-    console.log('Sever started successfully on port 3000!');
+app.listen(3001, () => {
+    console.log('Sever started successfully on port 3001!');
 })
 
 
--- frontend/src/user/Chat.jsx
+++ frontend/src/user/Chat.jsx
@@ -1,50 +1,82 @@
-import React, { useState, useEffect } from 'react'
-import Navbar from './Navbar'
+import React, { useState, useEffect } from 'react';
+import Navbar from './Navbar';
 import io from 'socket.io-client';
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const socket = io('http://localhost:3000/chat');
+const socket = io('http://localhost:3001');
 
 function Chat() {
     const [messages, setMessages] = useState([]);
-    const [message, setMessage] = useState('');
+    const [newMessage, setNewMessage] = useState('');
+    let sender = '1022';
+    let receiver = '555';
 
     useEffect(() => {
-        socket.on('chat message', (msg) => {
-            setMessages((prevMessages) => [...prevMessages, msg]);
-        });
-        
-        return () => {
-            socket.disconnect();};
-        }, []);
-        
+    // Listen for incoming messages
+    socket.on('chat message', (message) => {
+      setMessages((prevMessages) => [...prevMessages, message]);
+    });
+
+    // Clean up socket connection on component unmount
+    return () => {
+      socket.disconnect();
+    };
+    }, []);
+
     const handleSendMessage = () => {
-        socket.emit('chat message', message);
-        setMessage('');
+        socket.emit('chat message', { message: newMessage, sender, receiver });
+        setNewMessage('');
     };
 
-  return (
-      <>
-      <Navbar/>
-      <div className="BG" style={{width:'100%',backgroundColor:'#D9D9D9'}}>
-        <div className="container-xxl" style={{width:'1170px',backgroundColor:'red'}}>
+    return (
+        <>
+        <Navbar />
+        <div className="BG" style={{ width: '100%', backgroundColor: '#D9D9D9' }}>
+            <div className="container-xxl" style={{ width: '1170px', backgroundColor: 'red' }}>
             <div className="Chat">
-                <div className="contactlist">
-
-                </div>
+                <div className="contactlist"></div>
                 <div className="chatdetail">
-                    <div className="card-footer">
-                        <div className="input-group">
-                            <input type='text' className='form-control' id='message' placeholder='Send new message' value={message} onChange={(e) => setMessage(e.target.value)}/>
-                            <button className='btn btn-success' type='button' id='message' onClick={handleSendMessage} >Send</button>
-                        </div>
+                <div className="card-footer">
+                    <ul>
+                        {messages.map((message, index) => (
+                        <li key={index}>
+                            {message.sender === sender ? (
+                            <span>
+                                <strong>You:</strong> {message.message}
+                            </span>
+                            ) : (
+                            <span>
+                                <strong>{message.sender}:</strong> {message.message}
+                            </span>
+                            )}
+                        </li>
+                        ))}
+                    </ul>
+                    <div className="input-group">
+                    <input
+                        type='text'
+                        className='form-control'
+                        id='newMessage'
+                        placeholder='Send new message'
+                        value={newMessage}
+                        onChange={(e) => setNewMessage(e.target.value)}
+                    />
+                    <button
+                        className='btn btn-success'
+                        type='button'
+                        id='sendMessage'
+                        onClick={handleSendMessage}
+                    >
+                        Send
+                    </button>
                     </div>
                 </div>
+                </div>
             </div>
-        </div>    
-      </div>
-      </>
-  )
+            </div>
+        </div>
+        </>
+    );
 }
 
-export default Chat
+export default Chat;
