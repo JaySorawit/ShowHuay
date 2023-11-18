c6cc69bd5617ded6b258b52c2a1b46b8f62a7855
Jay Sorawit <sorawit.mokt@gmail.com>
2023-11-18 15:17:17 +0700

    fix

--- frontend/src/user/Login.jsx
+++ frontend/src/user/Login.jsx
@@ -19,7 +19,7 @@ function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://localhost:3000/login', {
+      const response = await fetch('http://localhost:3000/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
