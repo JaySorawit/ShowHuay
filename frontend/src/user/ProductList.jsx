ff4014f676eb2fb845cdad31061b65b5ca48ab9c
worapholohm <133252333+worapholohm@users.noreply.github.com>
2023-11-26 23:11:33 +0700

    Productlist
    Edit productlist

--- frontend/src/user/ProductList.jsx
+++ frontend/src/user/ProductList.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import Navbar from './Navbar'
-
+import '../css/Productlist.css'; // Import CSS file
+import Footer from './Footer';
 function ProductList() {
   return (
-    <>
-    <Navbar/>
-    <div>ProductList</div>
-    </>
+  <>
+  <Navbar/>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <div>Filter By</div>    
+        <label for="customRange1" class="form-label">Price</label>
+        <input type="range" class="form-range" id="customRange1"></input>
+      </div>
+    
+      <div className="col-9 col-9-gray">
+        <div>Search result for</div>     
+      </div>
+    
+    </div>
+  </div>
+  <Footer />
+  </>
   )
 }
 
