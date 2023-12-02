79bed20b87318224217c29f2385c48d44dc24362
Jay Sorawit <sorawit.mokt@gmail.com>
2023-12-02 16:12:59 +0700

    fix chat

--- backend/routes/accountRoutes.js
+++ backend/routes/accountRoutes.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 
-const { queryUsername, queryPurchases } = require("../controllers/accountController");
+const { getUserInfo, updateUserInfo, queryUsername, queryPurchases } = require("../controllers/accountController");
 
+router.route("/getuser/:userId").get(getUserInfo);
+router.route("/updateuser/:userId").get(updateUserInfo);
 router.route("/username/:productId").get(queryUsername);
 router.route("/purchases/:userId").get(queryPurchases);
 
